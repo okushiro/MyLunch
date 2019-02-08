72d945ad68da55c4d1b7777f267e35e840b28a36
奥城健太郎 <ken09460308@gmail.com>
2019-02-09 06:10:38 +0900

    Initial Commit

---
+++ MyLunch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyLunch
+//
+//  Created by 奥城健太郎 on 2019/02/09.
+//  Copyright © 2019 奥城健太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
